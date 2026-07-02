7e1ebc2373200a014ab7f4e5fca0080a3c529b92
man tuo <2428184991@qq.com>
2026-07-02 14:57:15 +0800

    Delete target/classes directory

--- target/classes/db/migration/V1__create_task_table.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE task (
-                      id BIGINT NOT NULL AUTO_INCREMENT,
-                      prompt TEXT NOT NULL,
-                      status VARCHAR(32) NOT NULL,
-                      created_at DATETIME(6) NOT NULL,
-                      updated_at DATETIME(6) NOT NULL,
-                      PRIMARY KEY (id)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
